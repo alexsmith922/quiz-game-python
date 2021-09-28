334792b7e49209bba57cf570ae6e63e09db2c36e
alexsmith922 <10asmith@gmail.com>
2021-09-28 09:22:03 -0500

    Moved Question to question_model.py Wrote for loop to populate data into question_bank

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-class Question:
-    def __init__(self, q_text, q_answer):
-        self.text = q_text
-        self.answer = q_answer
+from question_model import Question
+from data import question_data
+
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(q_text=question_text, q_answer=question_answer)
+    question_bank.append(new_question)
+
+print(question_bank)
